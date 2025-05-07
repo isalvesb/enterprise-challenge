6d2cc6438d36b7d563002a5f0ae393f4d1321357
Isa Alves <alvesisamara254@gmail.com>
2025-05-07 18:23:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleTheme() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
+}
+
+window.onload = () => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') document.body.classList.add('dark-mode');
+}
